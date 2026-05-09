16d90c19502420e2dee526893662afd54a0de07c
Chi Xu <1378004628@qq.com>
2026-05-09 17:25:24 +0000

    Update SurVis bibliography

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,116 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "balaz2023tensor": {
+        "author": "Bal\u00e1\u017e, M. and Tar\u00e1bek, P.",
+        "doi": "10.3390/app13031406",
+        "journal": "Applied Sciences",
+        "keywords": "Game AI Methods, Monte Carlo Tree Search, Reinforcement Learning",
+        "number": "3",
+        "pages": "1406",
+        "title": "Tensor Implementation of Monte-Carlo Tree Search for Model-Based Reinforcement Learning",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "volume": "13",
+        "year": "2023"
+    },
+    "browne2012survey": {
+        "author": "Browne, C. B. and Powley, E. and Whitehouse, D. and Lucas, S. M. and Cowling, P. I. and Rohlfshagen, P. and Tavener, S. and Perez, D. and Samothrakis, S. and Colton, S.",
+        "doi": "10.1109/TCIAIG.2012.2186810",
+        "journal": "IEEE Transactions on Computational Intelligence and AI in Games",
+        "keywords": "Game AI Methods, Monte Carlo Tree Search, Survey",
+        "number": "1",
+        "pages": "1--43",
+        "title": "A Survey of Monte Carlo Tree Search Methods",
+        "type": "article",
+        "volume": "4",
+        "year": "2012"
+    },
+    "denning2013control": {
+        "author": "Denning, Tamara and Lerner, Ada and Shostack, Adam and Kohno, Tadayoshi",
+        "booktitle": "Proceedings of the 2013 ACM SIGSAC Conference on Computer and Communications Security",
+        "doi": "10.1145/2508859.2516753",
+        "keywords": "Cybersecurity Awareness Games, Serious Games, Tabletop Games",
+        "pages": "915--928",
+        "title": "Control-Alt-Hack: The Design and Evaluation of a Card Game for Computer Security Awareness and Education",
+        "type": "inproceedings",
+        "year": "2013"
+    },
+    "furnell2026roll": {
+        "author": "Furnell, S. and \u0160mid, L. and Todd, J. and Carpent, X. and Castle-Green, S.",
+        "doi": "10.53735/cisse.v13i1.238",
+        "journal": "Journal of The Colloquium for Information Systems Security Education",
+        "keywords": "Cybersecurity Awareness Games, Cyber Defence Dice, Serious Games",
+        "number": "1",
+        "title": "Roll With It: Awareness Raising with Cyber Defence Dice",
+        "type": "article",
+        "volume": "13",
+        "year": "2026"
+    },
+    "gwenhure2024gamification": {
+        "author": "Gwenhure, A. K. and Rahayu, F. S.",
+        "doi": "10.17083/ijsg.v11i1.719",
+        "journal": "International Journal of Serious Games",
+        "keywords": "Cybersecurity Awareness Games, Gamification, Systematic Review",
+        "number": "1",
+        "title": "Gamification of Cybersecurity Awareness for Non-IT Professionals: A Systematic Literature Review",
+        "type": "article",
+        "volume": "11",
+        "year": "2024"
+    },
+    "hart2020riskio": {
+        "author": "Hart, S. and Margheri, A. and Paci, F. and Sassone, V.",
+        "doi": "10.1016/j.cose.2020.101827",
+        "journal": "Computers & Security",
+        "keywords": "Cybersecurity Awareness Games, Serious Games, Tabletop Games",
+        "pages": "101827",
+        "title": "Riskio: A Serious Game for Cyber Security Awareness and Education",
+        "type": "article",
+        "volume": "95",
+        "year": "2020"
+    },
+    "kim2024surrogate": {
+        "author": "Kim, M.-J. and Lee, D. and Kim, J. S. and Ahn, C. W.",
+        "doi": "10.1016/j.engappai.2024.108152",
+        "journal": "Engineering Applications of Artificial Intelligence",
+        "keywords": "Game AI Methods, Monte Carlo Tree Search, Real-Time Games",
+        "pages": "108152",
+        "title": "Surrogate-assisted Monte Carlo Tree Search for Real-Time Video Games",
+        "type": "article",
+        "volume": "133",
+        "year": "2024"
+    },
+    "ng2025cybersecurity": {
+        "author": "Ng, C. Y. and Hasan, M. K. B.",
+        "doi": "10.1016/j.cose.2024.104307",
+        "journal": "Computers & Security",
+        "keywords": "Cybersecurity Awareness Games, Serious Games, Systematic Review",
+        "pages": "104307",
+        "title": "Cybersecurity Serious Games Development: A Systematic Review",
+        "type": "article",
+        "volume": "150",
+        "year": "2025"
+    },
+    "scholefield2019gamification": {
+        "address": "Cham",
+        "author": "Scholefield, S. and Shepherd, L. A.",
+        "booktitle": "HCI for Cybersecurity, Privacy and Trust",
+        "doi": "10.1007/978-3-030-22351-9_13",
+        "editor": "Moallem, A.",
+        "keywords": "Cybersecurity Awareness Games, Gamification, User Awareness",
+        "pages": "191--201",
+        "publisher": "Springer",
+        "title": "Gamification Techniques for Raising Cyber Security Awareness",
+        "type": "incollection",
+        "year": "2019"
+    },
+    "zhangkennedy2021systematic": {
+        "author": "Zhang-Kennedy, L. and Chiasson, S.",
+        "doi": "10.1145/3427920",
+        "journal": "ACM Computing Surveys",
+        "keywords": "Cybersecurity Awareness Games, Multimedia Tools, Systematic Review",
+        "number": "1",
+        "pages": "1--39",
+        "title": "A Systematic Review of Multimedia Tools for Cybersecurity Awareness and Education",
+        "type": "article",
+        "volume": "54",
+        "year": "2021"
     }
 };
